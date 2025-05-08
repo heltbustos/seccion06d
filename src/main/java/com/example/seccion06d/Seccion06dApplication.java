89478018104b5e2288feeef8e0ad279ea651906c
Helton Bustos <helt.bustos@profesor.duoc.cl>
2025-05-08 13:47:29 -0400

    proyecto inicial

---
+++ src/main/java/com/example/seccion06d/Seccion06dApplication.java
@@ -0,0 +1,13 @@
+package com.example.seccion06d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seccion06dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seccion06dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seccion06d/Seccion06dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seccion06d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seccion06dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
